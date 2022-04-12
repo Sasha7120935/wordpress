a7ba5ab55a9cc6daca7ad5fdf077e37c6a35a67c
Sasha7120935 <belousalek@gmail.com>
2022-04-12 20:50:33 +0300

    create web4pro-ajax plugin 12.04

--- wp-content/plugins/web4pro-ajax/widget/web4pro-ajax-widget.php
+++ wp-content/plugins/web4pro-ajax/widget/web4pro-ajax-widget.php
@@ -16,27 +16,32 @@ class Web4proAjaxPostsWidget extends WP_Widget
      */
     public function widget($args, $instance)
     {
-        $title = apply_filters('widget_title', $instance['title']); // к заголовку применяем фильтр (необязательно)
+
+        $title = isset($instance['title']) ? $instance['title'] : '';
+        $date = isset($instance['date']) ? $instance['date'] : '';
         $posts_per_page = $instance['posts_per_page'];
-
-//        echo $args['before_widget'];
-
-//        if (!empty($title))
-//            echo $args['before_title'] . $title . $args['after_title'];
-
-        $q = new WP_Query("posts_per_page=$posts_per_page&orderby=comment_count");
-        if ($q->have_posts()):
+        $atts = [
+            'post_type' => 'post',
+            'orderby' => 'comment_count',
+            'title' => $instance['title'],
+            'posts_per_page' => $instance['posts_per_page'],
+            'date_query' => array(
+                'after' => '2021-11-04',
+                'before' => $date
+            ),
+        ];
+        $post_query = new WP_Query($atts);
+        if ($post_query->have_posts()):
             ?>
             <ul><?php
-            while ($q->have_posts()): $q->the_post();
+            while ($post_query->have_posts()): $post_query->the_post();
                 ?>
-                <li><a href="<?php the_permalink() ?>"><?php the_title() ?></a></li><?php
+                <li><a href="<?php the_permalink() ?>"><?php the_title() ?></a><br></li><?php
             endwhile;
             ?></ul><?php
         endif;
         wp_reset_postdata();
 
-//        echo $args['after_widget'];
     }
 
     /*
@@ -55,6 +60,24 @@ class Web4proAjaxPostsWidget extends WP_Widget
             $posts_per_page = $instance['posts_per_page'];
         }
         ?>
+        <script type="text/javascript">
+            jQuery(document).ready(function ($) {
+               let v = $('#widget-true_top_widget-14-title').on('change', function (event) {
+                    event.preventDefault();
+                    let val = $(this).val();
+                    $.ajax({
+                        url: '/wp-admin/widgets.php', // сделали запрос
+                        type: "POST", // указали метод
+                        data: { // передаем параметры отправляемого запроса
+                            action: 'my_ajax_action', // вызываем хук который обработает наш ajax запрос
+                            name: val, // передаем параметры из кнопки
+                        },
+                        success: function (data) {// получаем результат в переменной data
+                        }
+                    });
+                });
+            });
+        </script>
         <p>
             <label for="<?php echo $this->get_field_id('title'); ?>">Title</label>
             <input class="web4pro-title" id="<?php echo $this->get_field_id('title'); ?>"
@@ -83,6 +106,7 @@ class Web4proAjaxPostsWidget extends WP_Widget
     {
         $instance = array();
         $instance['title'] = (!empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
+        $instance['date'] = (!empty($new_instance['date'])) ? strip_tags($new_instance['date']) : '';
         $instance['posts_per_page'] = (is_numeric($new_instance['posts_per_page'])) ? $new_instance['posts_per_page'] : '5'; // по умолчанию выводятся 5 постов
         return $instance;
     }
