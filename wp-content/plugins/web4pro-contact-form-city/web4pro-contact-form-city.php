d8c1566182be29a62bd943f96b6bf396d2891245
Sasha7120935 <belousalek@gmail.com>
2022-05-17 22:10:07 +0300

    Implement a plugin that will allow, depending on the city field, to change the recipient's email (there may be more than one recipient).

--- wp-content/plugins/web4pro-contact-form-city/web4pro-contact-form-city.php
+++ wp-content/plugins/web4pro-contact-form-city/web4pro-contact-form-city.php
@@ -7,33 +7,57 @@ License: null
 
 add_action( 'wpcf7_before_send_mail', 'web_before_send_mail' );
 
-function web_before_send_mail( $contact_form )
+function web_before_send_mail($contact_form)
 {
 
     $submission = WPCF7_Submission::get_instance();
     $posted_data = $submission->get_posted_data();
     foreach ( $posted_data['city'] as $item => $key ) {
-        if ( $key == 'Kharkov' ) {
-            $recipient_email =  add_email();
-        } elseif ( $key == 'Kiev' ) {
-            $recipient_email =  add_email();
-        } elseif ( $key == 'Dnepr' ) {
-            $recipient_email =  add_email();
-        } elseif ( $key == 'Sumy' ) {
-            $recipient_email =  add_email();
-        } elseif ( $key == 'Mariupol' ) {
-            $recipient_email =  add_email();
+        if ( $key == '--select--' ) {
+            return $contact_form;
+        } elseif ( $key == get_post_meta(get_option( 'woocommerce_shop_page_id' ), 'city1', true ) ) {
+            $recipient_email = get_post_meta(get_option( 'woocommerce_shop_page_id' ), 'email1', true );
+        } elseif ( $key == get_post_meta(get_option( 'woocommerce_shop_page_id' ), 'city2', true ) ) {
+            $recipient_email = get_post_meta(get_option( 'woocommerce_shop_page_id' ), 'email2', true );
+        } elseif ( $key == get_post_meta(get_option( 'woocommerce_shop_page_id' ), 'city3', true ) ) {
+            $recipient_email = get_post_meta(get_option( 'woocommerce_shop_page_id' ), 'email3', true );
+        } elseif ( $key == get_post_meta(get_option( 'woocommerce_shop_page_id' ), 'city4', true ) ) {
+            $recipient_email = get_post_meta(get_option( 'woocommerce_shop_page_id' ), 'email4', true );
+        } elseif ( $key == get_post_meta(get_option( 'woocommerce_shop_page_id' ), 'city5', true ) ) {
+            $recipient_email = get_post_meta(get_option( 'woocommerce_shop_page_id' ), 'email5', true );
         } else {
             return $contact_form;
         }
     }
     $properites = $contact_form->get_properties();
     $properites['mail']['recipient'] .= ', ' . $recipient_email;
-
-    $contact_form->set_properties( $properites );
-    return ( $contact_form );
+    $contact_form->set_properties($properites);
+    return ($contact_form);
 }
-function add_email(){
-    $email = 'test@gmail.com';
-    return $email;
+
+/**
+ * Show or do not show custom contact form on page
+ */
+add_action( 'woocommerce_before_main_content', 'add_field' );
+function add_field()
+{
+
+    if ( open_field() == 'Yes' ) {
+        echo get_field( 'custom_contact', get_option( 'woocommerce_shop_page_id' ) );
+    } else {
+        echo '';
+    }
+
+}
+
+/**
+ * @return mixed|void
+ */
+function open_field()
+{
+    $field = get_post_meta(get_option( 'woocommerce_shop_page_id' ), 'open_or_close', true );
+    foreach ( $field as $item ) {
+        return $item;
+    }
+
 }
---
+++ wp-content/themes/storefront/archive.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying archive pages.
+ *
+ * Learn more: https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package storefront
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+
+	</div><!-- #primary -->
+
+<?php
+do_action( 'storefront_sidebar' );
+get_footer();
